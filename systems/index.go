80120aa743ad24f2639ea4793e9a459d7cf607a1
Christian Frauenstein <cfrauenstein@gmail.com>
2018-08-19 14:20:20 +0200

    Got a basic tweening system going.

--- systems/index.go
+++ systems/index.go
@@ -6,58 +6,11 @@ import (
 	"time"
 	"towers/components"
 	"towers/entities"
-	"towers/systems"
 
 	"engo.io/ecs"
 	"engo.io/engo"
 	"engo.io/engo/common"
 )
-
-type TowerCreator struct {
-}
-
-// CreateTower make new towers
-func (tc *TowerCreator) CreateTower(tb *systems.TowerCreateSystem) {
-
-	tower := entities.Tower{BasicEntity: ecs.NewBasic()}
-
-	tower.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{X: tb.mouseTracker.MouseX, Y: tb.mouseTracker.MouseY},
-		Width:    10,
-		Height:   10,
-	}
-
-	tower.RenderComponent = common.RenderComponent{
-		Drawable: common.Circle{
-			BorderColor: color.White,
-			BorderWidth: 0,
-		},
-	}
-
-	tower.TweenComponent = components.TweenComponent{
-		StartPosition:       tower.SpaceComponent.Position,
-		DestinationPosition: engo.Point{X: 500, Y: 500},
-		StartTime:           time.Now(),
-		EndTime:             time.Now().Add(time.Duration(2 * time.Second)),
-		Tweening:            true,
-	}
-	//
-	// fmt.Println(tower.TweenComponent.StartTime.String())
-	// fmt.Println(tower.TweenComponent.EndTime.String())
-	// fmt.Println(time.Duration(2 * time.Second).Nanoseconds())
-	// fmt.Println(tower.TweenComponent.EndTime.Sub(tower.TweenComponent.StartTime).Nanoseconds())
-
-	for _, system := range tb.world.Systems() {
-		switch sys := system.(type) {
-		case *common.RenderSystem:
-			sys.Add(&tower.BasicEntity, &tower.RenderComponent, &tower.SpaceComponent)
-		case *LinearTweenSystem:
-			sys.Add(&tower.BasicEntity, &tower.SpaceComponent, &tower.TweenComponent)
-		}
-
-	}
-
-}
 
 //MouseTracker tracker entity
 type MouseTracker struct {
@@ -71,7 +24,7 @@ type TowerCreateSystem struct {
 	mouseTracker MouseTracker
 }
 
-// Remove is called whenever an Entity is removed from the World, in order to remove it from this sytem as well
+//Remove is called whenever an Entity is removed from the World, in order to remove it from this sytem as well
 func (*TowerCreateSystem) Remove(ecs.BasicEntity) {}
 
 // Update is ran every frame, with `dt` being the time
@@ -137,7 +90,6 @@ func (tb *TowerCreateSystem) New(w *ecs.World) {
 			sys.Add(&tb.mouseTracker.BasicEntity, &tb.mouseTracker.MouseComponent, nil, nil)
 		}
 	}
-
 }
 
 //LinearTweenEntity is an example of a entity that needs to tween
@@ -215,5 +167,4 @@ func (lts *LinearTweenSystem) Update(dt float32) {
 	}
 
 	// fmt.Println(len(lts.entities))
-
 }
